4f7652e951a271e88242a0b398b10b885521707e
Alex Schneider <94626498+AlexandruSchneider@users.noreply.github.com>
2022-01-08 11:57:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,32 +9,32 @@
 # =============================================================================
 # Imports
 # =============================================================================
-import os
-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+def main():
+    external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+    app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+    app.layout = html.Div([
+        html.H2('Hello World'),
+        dcc.Dropdown(
+            id='dropdown',
+            options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
+            value='LA'
+        ),
+        html.Div(id='display-value')
+    ])
 
-server = app.server
+    @app.callback(dash.dependencies.Output('display-value', 'children'),
+                  [dash.dependencies.Input('dropdown', 'value')])
+    def display_value(value):
+        return 'You have selected "{}"'.format(value)
 
-app.layout = html.Div([
-    html.H2('Hello World'),
-    dcc.Dropdown(
-        id='dropdown',
-        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
-        value='LA'
-    ),
-    html.Div(id='display-value')
-])
+    return app, app.server
 
-@app.callback(dash.dependencies.Output('display-value', 'children'),
-                [dash.dependencies.Input('dropdown', 'value')])
-def display_value(value):
-    return 'You have selected "{}"'.format(value)
 
+app, server = main()
 if __name__ == '__main__':
     app.run_server(debug=True)
